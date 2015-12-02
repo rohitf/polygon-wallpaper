b47a64ebc08c0aef5d57674b26fadaf37e08e4a7
Rohit Falor <rohitfalor@outlook.com>
2015-12-02 01:34:29 -0800

    Polish UI

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
 var canvas = document.getElementById('canvas');
 
+function initialize(){
+    for (var i = 1; i <= 1000; i++) {
+        ctx.beginPath();
+        if (left) {
+            ctx.moveTo(0, randomize(canvas.height + 10));
+            left = false;
+        } else {
+            ctx.moveTo(randomize(canvas.width+ 10), 0);
+            left = true;
+        }
+        ctx.lineTo(randomize(canvas.width + 50), randomize(canvas.height  + 50));
+        ctx.lineTo(randomize(canvas.width + 50), randomize(canvas.height + 50));
+        ctx.fillStyle = getRndColor();
+        ctx.fill();
+    }
+}
+
 function randomize(num) {
     var a = Math.floor(Math.random() * num);
     return a;
@@ -19,31 +36,39 @@ function getRndColor() {
     return final;
 }
 
-$('*').click(function () {
-    clearInterval(timer);
-    $('button').show();
-});
 
-$('button').click(function () {
-    $(this).css('left', '-400px');
-    $('h3').text("Right click and save a beautiful background!");
+$('#save').click(function () {
+    clearInterval(timer);
+
+    //Convert canvas to image    
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     var dataURL = canvas.toDataURL();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     document.getElementById('canvasImg').src = dataURL;
-    $('#canvasImg').css('border', '5px solid black');
     $('#canvas').hide();
     $('#canvasImg').show();
+    
+    // //Download image
+    // var a = document.createElement('a');
+    // a.href = dataURL;
+    // a.download = "output.png";
+    // document.body.appendChild(a);
+    // console.log(a)
+    // a.click();
+    $('h4').text("Right click on the image to save a beautiful wallpaper!")
 });
 
 if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
     var left = true;
 
-    canvas.height = 495;
-	  canvas.width = canvas.height * 1.76;
-  
+    canvas.height = screen.height - 90;
+	canvas.width = screen.width + 20;
+    
+    initialize();
+    initialize();
+
     var timer = setInterval(function () {
         for (var i = 1; i <= 1000; i++) {
             ctx.beginPath();
@@ -59,5 +84,5 @@ if (canvas.getContext) {
             ctx.fillStyle = getRndColor();
             ctx.fill();
         }
-    }, 1000);
+    }, 5000);
 }
